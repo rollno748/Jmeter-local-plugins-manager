843024b6535116ffc82058984fe24d7dc8b8aafe
Mohamed Ibrahim <mibrahim@localhost.perftalks>
2019-06-21 10:27:59 +0530

    Added Hashmap Change

--- jmeter-local-plugins-manager/src/main/java/com/perftalks/jmeter/app/Initialize.java
+++ jmeter-local-plugins-manager/src/main/java/com/perftalks/jmeter/app/Initialize.java
@@ -1,12 +1,36 @@
 package com.perftalks.jmeter.app;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.util.Properties;
+import java.util.logging.Logger;
+
+import org.json.JSONArray;
+
+import com.perftalks.jmeter.repo.rest.HTTPRequests;
+import com.perftalks.jmeter.repo.utils.DirOps;
+import com.perftalks.jmeter.repo.utils.LoadMap;
+import com.perftalks.jmeter.repo.utils.Plugins;
 
 public class Initialize {
 
-	public static void setParams(Properties props) {
-		// TODO Auto-generated method stub
+	private static final Logger LOGGER = Logger.getLogger(Initialize.class.getName());
+	private static LoadMap load = new LoadMap();
+	
+
+	public static void setParams(Properties props) throws MalformedURLException, IOException {
 		
+		if(DirOps.createDir(props.getProperty("LOCAL_REPO_PATH"))) {
+			JSONArray jmeterJson = HTTPRequests.get(props.getProperty("JMETER_REPO_URL"));
+			if(!jmeterJson.isEmpty()) {
+				LOGGER.info("Checking available plugins");
+				Plugins.getMissingPlugins(load.ConvertToMap(jmeterJson), props);
+				
+			}else {
+				System.exit(1);
+			}
+		}
+	
 	}
 
 }
--- jmeter-local-plugins-manager/src/main/java/com/perftalks/jmeter/app/PluginsManager.java
+++ jmeter-local-plugins-manager/src/main/java/com/perftalks/jmeter/app/PluginsManager.java
@@ -6,19 +6,26 @@ import java.util.Properties;
 import java.util.logging.Logger;
 
 public class PluginsManager {
-	
-	private static Properties props = new Properties();
-	
+
 	private static final Logger LOGGER = Logger.getLogger(PluginsManager.class.getName());
 
+	private static Properties props = new Properties();
+
 	public static void main(String[] args) throws IOException {
-		
-		LOGGER.info("Starting Local Plugins Manager");
-		InputStream inputStream = PluginsManager.class.getClassLoader().getResourceAsStream("config.properties");
-		props.load(inputStream);
-		
-		Initialize.setParams(props);
-		
+
+		try {
+			InputStream inputStream = PluginsManager.class.getClassLoader().getResourceAsStream("config.properties");
+			props.load(inputStream);
+			LOGGER.info("Loading Properties completed");
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+
+
+		if(!props.isEmpty()) {
+			LOGGER.info("Starting Local Plugins Manager");
+			Initialize.setParams(props);
+		}
 
 	}
 
